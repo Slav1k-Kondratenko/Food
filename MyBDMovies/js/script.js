957bcf662d90da91bc12ce3c3b0759af8acc523a
Slav1k <Slav1_k@ukr.net>
2021-08-25 23:38:28 +0300

    worked with elements

--- MyBDMovies/js/script.js
+++ MyBDMovies/js/script.js
@@ -16,7 +16,7 @@
 
 const movieDB = {
     movies: [
-        "Логан",
+        "666Логан",
         "Лига справедливости",
         "Ла-ла лэнд",
         "Одержимость",
@@ -24,3 +24,35 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = document.querySelector('.promo__genre'),
+    movieslist = document.querySelector('.promo__interactive-list');
+
+function deleteAdv(arr) {
+    arr.forEach((element) => {
+        element.remove();
+    });
+}
+
+function makeChange() {
+    poster.style.backgroundImage = 'url("img/bg.jpg")';
+    genre.textContent = 'Драма';
+}
+
+function creatListFilms(films, parent) {
+    parent.innerHTML = '';
+    films.sort();
+
+    films.forEach((film, i) => {
+        parent.innerHTML += `
+        <li class="promo__interactive-item">${i +1}.  ${film}
+        <div class="delete"></div>
+        </li>
+        `;
+    });
+}
+
+deleteAdv(adv);
+makeChange();
+creatListFilms(movieDB.movies, movieslist);
